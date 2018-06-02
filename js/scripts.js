a6f29ed005cbd9bb89eacac28302d4baf3f4621b
jacqui <gitaujaquiline@gmail.com>
2018-06-02 09:39:58 +0300

    Add the Nav menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function openNav() {
-    document.getElementById("humMenu").style.width = "100%";
+    document.getElementById("myNav").style.width = "100%";
   }
 
 function closeNav() {
-    document.getElementById("humMenu").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%";
   }
